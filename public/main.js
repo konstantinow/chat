e479ccb9f9ad64f1ca5faf86a9a4adcd8dde3a88
konstantinow <vitaliy.dpiki@gmail.com>
2017-12-07 11:06:00 +0300

    Should work

--- public/main.js
+++ public/main.js
@@ -64,7 +64,7 @@ $(function() {
         message: message
       });
       // tell server to execute 'new message' and send along one parameter
-      socket.emit('new message', message);
+      socket.emit('new message', {username: username, message: message});
     }
   }
 
